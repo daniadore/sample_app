3fd00f5583ece531c789dda1728cd91515eb74c2
daniadore <daniadore13@gmail.com>
2021-08-30 12:19:14 +0800

    chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,29 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save {self.email = email.downcase}
-  validates :name, presence: true
-  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  #validates name
+  validates :name, presence: true
+  #validates email
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
   has_secure_password
+
+  #email lowercase  conversion
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
+
+  # Returns the hash digest of the given string.
+  def User.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+      BCrypt::Engine::MIN_COST
+    else
+      BCrypt::Engine.cost
+    end
+      BCrypt::Password.create string, cost: cost
+  end
 end
